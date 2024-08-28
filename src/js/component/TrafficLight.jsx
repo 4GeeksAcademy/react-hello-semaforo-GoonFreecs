8e86a74c9305acc164149876bb435a95b4b3d3b4
Gonzalo Cortés <gonzaloigcortescortes@gmail.com>
2024-08-28 03:20:25 +0000

    finalizado basico

---
+++ src/js/component/TrafficLight.jsx
@@ -0,0 +1,27 @@
+import React, { useState } from "react";
+
+const TrafficLight = () => {
+    const [selectedColor, setSelectedColor] = useState("green");
+    return (
+        <div className="trafic-light">
+            <div className="support"></div>
+            <div
+            onClick={() =>setSelectedColor("red")}
+             className={"light red" + (selectedColor === "red" ? " glow" : "")
+             }></div>
+            <div
+            onClick={() =>setSelectedColor("yellow")}
+             className={"light yellow" + (selectedColor === "yellow" ? " glow" : "")
+
+             }></div>
+            <div
+            onClick={() =>setSelectedColor("green")}
+             className={"light green" + (selectedColor === "green" ? " glow" : "")
+
+             }></div>
+        </div>
+
+    )
+}
+
+export default TrafficLight;
